a7bfda4db23075c98e9ee095f90b804e68674351
sarakhatun <sarakhatun.breakthrough@gmail.com>
2023-03-24 15:25:44 +0000

    we found that Revenue and MetaScore are the only columns with missing entries, so we removed rows with the missing  values

--- Project.py
+++ Project.py
@@ -5,4 +5,7 @@ import seaborn as sns
 
 df_movie = pd.read_csv("IMDB-Movie-Data.csv")
 
+# we found that Revenue and MetaScore are the only columns with missing entries, so we removed rows with the missing  values
+
+df_movie = df_movie.dropna(axis=0, how='any')
 df_movie.info()
