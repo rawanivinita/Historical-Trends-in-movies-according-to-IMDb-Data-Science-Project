d34cbc9d6c905e30aad15e3df3adbdb452c83ce1
sarakhatun <sarakhatun.breakthrough@gmail.com>
2023-03-24 15:04:07 +0000

    i added the packages and run the dataframe

--- Project.py
+++ Project.py
@@ -1,1 +1,8 @@
-import matplotlib_inline;
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib
+import seaborn as sns
+
+df_movie = pd.read_csv("IMDB-Movie-Data.csv")
+
+df_movie.info()
