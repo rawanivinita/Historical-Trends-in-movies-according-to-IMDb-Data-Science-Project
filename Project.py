e76bca7c1421110fc42e57100638cf2c6b87872d
sarakhatun <sarakhatun.breakthrough@gmail.com>
2023-03-28 17:28:39 +0100

    I added the unit of the revenue

--- Project.py
+++ Project.py
@@ -6,24 +6,26 @@ import numpy as np
 
 df_movie = pd.read_csv("IMDB-Movie-Data.csv")
 
-# we found that Revenue and MetaScore are the only columns with missing entries, so we removed rows with the missing  values
+#we found that Revenue and MetaScore are the only columns with missing entries,so we removed rows with the missing values
 
 df_movie = df_movie.dropna(axis=0, how='any')
 df_movie.info()
+
+#
 
 #Scatter Plot between Year and Revenue
 plt.scatter( df_movie['Year'],df_movie['Revenue (Millions)'])
 plt.title('Revenue (Millions) vs Year')
 plt.xlabel('Year')
 plt.ylabel('Revenue (Millions)')
-#plt.show()
+plt.show()
 
 #Scatter Plot between Runtime and Revenue
 plt.scatter( df_movie['Runtime (Minutes)'],df_movie['Revenue (Millions)'])
-plt.title('Revenue vs Runtime (Minutes)')
+plt.title('Revenue (Millions) vs Runtime (Minutes)')
 plt.xlabel('Runtime (Minutes)')
-plt.ylabel('Revenue')
-#plt.show()
+plt.ylabel('Revenue (Millions)')
+plt.show()
 
 
 #Scatter Plot between Year and Rating
@@ -31,21 +33,21 @@ plt.scatter( df_movie['Year'],df_movie['Rating'])
 plt.title('Rating vs Year')
 plt.xlabel('Year')
 plt.ylabel('Rating')
-#plt.show()
+plt.show()
 
 #Scatter Plot between Runtime and Rating
 plt.scatter( df_movie['Runtime (Minutes)'],df_movie['Rating'])
 plt.title('Runtime (Minutes) vs Rating')
 plt.xlabel('Runtime (Minutes)')
 plt.ylabel('Rating')
-#plt.show()
+plt.show()
 
 #Scatter Plot between Revenue and Rating
 plt.scatter( df_movie['Rating'],df_movie['Revenue (Millions)'])
 plt.title('Revenue (Millions) vs Rating')
 plt.xlabel('Rating')
 plt.ylabel('Revenue (Millions)')
-#plt.show()
+plt.show()
 
 
 # print the correlation
